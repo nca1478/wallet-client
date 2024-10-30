b5781e73b86620220ae42bb247d67b9b99bfcb2f
Nelson Cadenas <nca1478@yahoo.com>
2024-10-30 12:49:58 -0400

    deleted: env var JWT_SEED

--- src/config/envs.config.ts
+++ src/config/envs.config.ts
@@ -3,6 +3,5 @@ import { get } from "env-var";
 
 export const envs = {
   PORT: get("PORT").required().asPortNumber(),
-  JWT_SEED: get("JWT_SEED").required().asString(),
   SOAP_BASE_URL: get("SOAP_BASE_URL").required().asUrlString(),
 };
