a57720c403dfe499a0eeeb30e5190b171e57bf43
Nelson Cadenas <nca1478@yahoo.com>
2024-11-07 23:10:48 -0400

    testing: docker files (prod)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import "reflect-metadata";
 import express from "express";
+import cors from "cors";
 import { AppRoutes } from "./routes";
 import { envs } from "./config";
 
 const app = express();
 const port = envs.PORT;
 
+app.set("port", port);
+app.use(cors());
 app.use(express.json());
 app.use(AppRoutes.routes);
 
 app.listen(port, () => {
-  console.log(`REST API client running on http://localhost:${port}/api`);
+  console.log(`REST API client running on port ${port}`);
 });
